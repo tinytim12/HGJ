1b6ebd3c3f91c2fa9ae318fef82170259cb55e3d
Timothy Yeo <evilsengokunadeko@gmail.com>
2020-08-03 03:03:36 +0800

    Committing...

--- YarnDemo/Assets/Scripts/GameController.cs
+++ YarnDemo/Assets/Scripts/GameController.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 
-public class GameManager : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     public int day;
     // Start is called before the first frame update
 
     void Awake() {
+        day = 1;
         DontDestroyOnLoad(this.gameObject);
     }
 
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     public void switchToHallway() {
         SceneManager.LoadScene("Hallway");
+    }
+
+    [YarnCommand("getDay")]
+    public int getDay() {
+        return day;
     }
 }
