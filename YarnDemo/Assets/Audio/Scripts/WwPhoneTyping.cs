c88cd17bd6101ef9abb1a726566c1c1015517163
Maskedsound <contact@maskedsound.com>
2020-08-03 02:08:38 +0800

    audio

---
+++ YarnDemo/Assets/Audio/Scripts/WwPhoneTyping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class WwPhoneTyping : MonoBehaviour
+{
+    public string PhoneStart = "Phone_Typing_Start";
+    public string PhoneStop = "Phone_Typing_Stop";
+
+    Yarn.Unity.DialogueUI DialogueUI;
+
+    [YarnCommand("phoneTyping")]
+    public void PhoneTyping()
+    {
+        AkSoundEngine.PostEvent(PhoneStart, gameObject);
+        DialogueUI.textSpeed = 0f;
+        
+    }
+
+    [YarnCommand("phoneTypingStop")]
+    public void PhoneTypingStop()
+    {
+        AkSoundEngine.PostEvent(PhoneStop, gameObject);
+    }
+}
---
+++ YarnDemo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PHONE_TYPING_START = 1019347030U;
+        static const AkUniqueID PHONE_TYPING_STOP = 1714883926U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID DIALOGUE = 3930136735U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
