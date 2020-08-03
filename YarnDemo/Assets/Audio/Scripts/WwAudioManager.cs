e2d818ce1e446a198ce7b0dffbc6f83ed29975fc
Maskedsound <contact@maskedsound.com>
2020-08-04 03:54:43 +0800

    audio stuff (need help with implementing later on)

--- YarnDemo/Assets/Audio/Scripts/WwAudioManager.cs
+++ YarnDemo/Assets/Audio/Scripts/WwAudioManager.cs
@@ -5,30 +5,34 @@ using Yarn.Unity;
 
 public class WwAudioManager : MonoBehaviour
 {
+    public GameObject audioManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //BGMusic();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     [YarnCommand("MainMenuMusic")]
     public void MainMenuMusic()
     {
-        AkSoundEngine.PostEvent("Main_menu_music", gameObject);
+        AkSoundEngine.PostEvent("Main_menu_music", audioManager);
         AkSoundEngine.SetState("Menu_or_BG", "Menu");
+        
     }
 
     [YarnCommand("BGMusic")]
     public void BGMusic()
     {
-        AkSoundEngine.PostEvent("BG_music", gameObject);
+        AkSoundEngine.PostEvent("BG_music", audioManager);
         AkSoundEngine.SetState("Menu_or_BG", "BG");
+        MonologueMusic();
+        Debug.Log("background music plays");
         
     }
 
@@ -36,5 +40,6 @@ public class WwAudioManager : MonoBehaviour
     public void MonologueMusic()
     {
         AkSoundEngine.SetState("Mon", "Monologue");
+        Debug.Log("monologue music plays");
     }
 }
