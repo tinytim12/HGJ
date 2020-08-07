ec588296ce000818fa42651b1e9381dec27a7347
Maskedsound <contact@maskedsound.com>
2020-08-07 13:36:05 +0800

    music manager script change

--- YarnDemo/Assets/Audio/Scripts/WwMusicManager.cs
+++ YarnDemo/Assets/Audio/Scripts/WwMusicManager.cs
@@ -22,6 +22,7 @@ public class WwMusicManager : MonoBehaviour
     public void MainMenuMusic()
     {
         AkSoundEngine.SetState("Menu_or_BG", "Menu"); // sets it to menu
+        AkSoundEngine.PostEvent("Menu_music", MusicManager);
 
     }
 
@@ -31,13 +32,24 @@ public class WwMusicManager : MonoBehaviour
         AkSoundEngine.SetState("Monologue_or_Mrs_Thiru", "Monologue");
     }
 
+    public void BGMusic()
+    {
+        AkSoundEngine.SetState("Menu_or_BG", "BG");
+        AkSoundEngine.PostEvent("BG_music", MusicManager);
+    }
+
+    public void BGMusicStop()
+    {
+        AkSoundEngine.SetState("Menu_or_BG", "BG");
+        AkSoundEngine.PostEvent("BG_music_stop", MusicManager);
+    }
     //Mrs Thiru
     public void MrsThiruMusicDay1()
     {
         //day 1
-        AkSoundEngine.SetState("Menu_or_BG", "BG");
         AkSoundEngine.SetState("Days", "Day_1");
         AkSoundEngine.SetState("Monologue_or_Mrs_Thiru", "Mrs_Thiru");
+        BGMusic();
    
     }
 
@@ -45,19 +57,19 @@ public class WwMusicManager : MonoBehaviour
     //choice: "I'm sorry"
     public void MrsThiruChoiceSorry()
     {
-        AkSoundEngine.SetState("Menu_or_BG", "BG");
         AkSoundEngine.SetState("Days", "Day_1");
         AkSoundEngine.SetState("Monologue_or_Mrs_Thiru", "Mrs_Thiru");
         AkSoundEngine.SetState("Mrs_Thiru_choices", "Hopeful");
+        BGMusic();
     }
 
     //choice: "Let's discuss
     public void MrsThiruDiscuss()
     {
-        AkSoundEngine.SetState("Menu_or_BG", "BG");
         AkSoundEngine.SetState("Days", "Day_1");
         AkSoundEngine.SetState("Monologue_or_Mrs_Thiru", "Mrs_Thiru");
         AkSoundEngine.SetState("Mrs_Thiru_choices", "Concerned");
+        BGMusic();
     }
 
     //choice: "I don't really care
@@ -68,4 +80,29 @@ public class WwMusicManager : MonoBehaviour
         AkSoundEngine.SetState("Monologue_or_Mrs_Thiru", "Mrs_Thiru");
         AkSoundEngine.SetState("Mrs_Thiru_choices", "Disappointed");
     }
+
+    //James day 1
+    public void JamesMusicDay1()
+    {
+        AkSoundEngine.SetState("Days", "Day_1");
+        AkSoundEngine.SetState("James_or_Siti", "James");
+        BGMusic();
+    }
+
+    //james choice: "actually I do"
+    public void JamesMoney()
+    {
+        AkSoundEngine.SetState("Days", "Day_1");
+        AkSoundEngine.SetState("James_choices", "Hopeful");
+    }
+
+    //james choice: "I'm sorry, I don't"
+    public void JamesNoMoney()
+    {
+        AkSoundEngine.SetState("Days", "Day_1");
+        AkSoundEngine.SetState("James_choices", "Disappointed");
+    }
+
+    //Siti day 1
+
 }
